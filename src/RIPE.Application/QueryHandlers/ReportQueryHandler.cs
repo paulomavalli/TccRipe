e42670db94606f8e0a371a22c6fc0d116d6362b3
Paulo Vinicius de Souza Mavalli - PMA <paulo.mavalli@iteris.com.br>
2020-07-14 21:11:23 -0300

    ajustando logica e strings do report

--- src/RIPE.Application/QueryHandlers/ReportQueryHandler.cs
+++ src/RIPE.Application/QueryHandlers/ReportQueryHandler.cs
@@ -55,6 +55,10 @@ namespace RIPE.Application.QueryHandlers
 
             try
             {
+                int somaRespostas = quantityPositiveAnswer + quantityNullableAnswer + quantityNegativeAnswer;
+
+                if (somaRespostas < 140) quantityNullableAnswer = 140 - somaRespostas;
+
                 decimal PerCentOkAsnwer = (decimal)0.714 * quantityPositiveAnswer;
                 decimal PerCentNullableAsnwer = (decimal)0.714 * quantityNullableAnswer;
                 decimal PerCentNegativeAsnwer = (decimal)0.714 * quantityNegativeAnswer;
@@ -64,8 +68,9 @@ namespace RIPE.Application.QueryHandlers
                 if (PerCentOkAsnwer >= 89)
                 {
                     habits = new BestHabits(new List<string> {
-                                                                "1",
-                                                                "2"
+                                                                " <li>   Permanecer em constante otimização; </li> <br>" +
+                                                                " <li>   Atualização periódica da documentação e eventos que vierem a surgir; </li> <br>"+
+                                                                " <li>   Definir um plano de continuidade, afim de permanecer com o nível 5 de maturidade. </li> <br>"
                                                               });
                     response.NivelMaturidade = "5";
                     response.Recomendacoes = habits;
@@ -73,8 +78,12 @@ namespace RIPE.Application.QueryHandlers
                 else if (PerCentOkAsnwer >= 49 && PerCentOkAsnwer < 89)
                 {
                     habits = new BestHabits(new List<string> {
-                                                                "1",
-                                                                "2"
+                                                                " <li>    Os requisitos do processo não se limitam as necessidades atuais, é necessária uma visão de futuro; </li> <br>"
+                                                                +" <li>    Os treinos realizados internamente acabam gerando boas práticas para o uso de outras empresas; </li> <br>"
+                                                                +" <li>    Procedimentos e técnicas precisam estar bem estabelecidos para que boas práticas externas sejam aplicadas; </li> <br>"
+                                                                +" <li>    É necessário o uso intensivo de tecnologia para consolidar práticas sofisticadas; </li> <br>"
+                                                                +" <li>    A avaliação de desempenho é utilizada em todos os aspectos, inconsistências são notadas e corrigidas rapidamente quando a causa raiz é encontrada; </li> <br>"
+                                                                +" <li>    Necessária a utilização de ajuda externa na melhoria dos processos. </li> <br>"
                                                               });
                     response.NivelMaturidade = "4";
                     response.Recomendacoes = habits;
@@ -82,8 +91,12 @@ namespace RIPE.Application.QueryHandlers
                 else if (PerCentOkAsnwer > 14 && PerCentOkAsnwer < 49)
                 {
                     habits = new BestHabits(new List<string> {
-                                                                "1",
-                                                                "2"
+                                                                " <li>    Requisitos do processo são compreendidos plenamente; </li> <br>"
+                                                               +" <li>    Treinos formais precisam suportar um programa de capacitação do funcionário; </li> <br>"
+                                                               +" <li>    Papéis e responsabilidades são definidos e documentados; </li> <br>"
+                                                               +" <li>    Técnicas e ferramentas formam uma abordagem tática; </li> <br>"
+                                                               +" <li>    É necessário medir o desempenho destes processos+ analisar a causa raiz de problemas e identificar as inconsistências; </li> <br>"
+                                                               +" <li>    Envolvimento de todos os funcionários relevantes para o setor. </li> <br>"
                                                               });
                     response.NivelMaturidade = "3";
                     response.Recomendacoes = habits;
@@ -91,8 +104,12 @@ namespace RIPE.Application.QueryHandlers
                 else if (PerCentOkAsnwer > 8 && PerCentOkAsnwer <= 14)
                 {
                     habits = new BestHabits(new List<string> {
-                                                                "1",
-                                                                "2"
+                                                                " <li>    Compreensão da necessidade dos processos e as ações necessárias para agir; </li> <br>"
+                                                               +" <li>    Treinos informais são feitos e auxiliam na formação individual do profissional; </li> <br>"
+                                                               +" <li>    Práticas necessitam ser padronizadas e documentadas; </li> <br>"
+                                                               +" <li>    Ferramentas utilizadas são padronizadas; </li> <br>"
+                                                               +" <li>    Avaliação de desempenho começa a surgir+ porém análise dos problemas ainda é intuitiva; </li> <br>"
+                                                               +" <li>    Apenas profissionais de TI são envolvidos nos processos. </li> <br>"
                                                               });
                     response.NivelMaturidade = "2";
                     response.Recomendacoes = habits;
@@ -101,9 +118,12 @@ namespace RIPE.Application.QueryHandlers
                 { 
                     
                     habits = new BestHabits (new List<string> {
-                                                                "1",
-                                                                "2" 
-                                                              });
+                                                                " <li>    Necessária a atenção para com as necessidades do processo; </li> <br>"
+                                                               +" <li>    Necessária a troca de informações a respeito das necessidades e problemas encontrados; </li> <br>"
+                                                               +" <li>    Maneiras de abordar os problemas acabam surgindo intuitivamente de várias fontes; </li> <br>"
+                                                               +" <li>    Ferramentas passam a surgir com o intuito de resolver o problema; </li> <br>"
+                                                               +" <li>    Ainda não é estabelecida a avaliação do desempenho. </li> <br>" 
+                                                               });
                     response.NivelMaturidade = "1";
                     response.Recomendacoes = habits;
                 }
